925666cecef10de5e3f3206e82fbe144e19fb4da
ewhd <ewhd22@gmail.com>
2024-03-14 13:28:58 -0700

    fixes bootstrap process

--- .config/ewhd/install-script.sh
+++ .config/ewhd/install-script.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # add backports to apt and install emacs (to get emacs 29)
-printf "\n\n# Add Debian Backports:\ndeb http://deb.debian.org/debian bookworm-backports main\n" >> /etc/apt/sources.list
-apt-get update
-apt install -t bookworm-backports emacs
+if ! grep -q "deb http://deb.debian.org/debian bookworm-backports main" /etc/apt/sources.list
+then
+    printf "\n\n# Add Debian Backports:\ndeb http://deb.debian.org/debian bookworm-backports main\n" >> /etc/apt/sources.list
+    apt-get update
+    apt install -t bookworm-backports emacs
+else
+    apt-get update
+fi
 
 
 # Install basic packages:
@@ -16,8 +21,7 @@ apt-get -y --ignore-missing install $(< basic-packages.list)
 
 # Change shell:
 if grep -q /bin/zsh /etc/shells; then
-    chsh /bin/zsh ewhd
-    printf "\nShell changed to zsh\n"
+    chsh -s /bin/zsh ewhd && printf "\nShell changed to zsh\n"
 fi
 
 #EOF
